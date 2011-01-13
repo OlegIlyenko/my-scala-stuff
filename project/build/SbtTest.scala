15dbfb57b169ee3cb52e6ff7dab2f88c52182e3c
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2011-01-14 00:42:02 +0100

    scalaz added and some initial setup

--- project/build/SbtTest.scala
+++ project/build/SbtTest.scala
@@ -3,7 +3,17 @@ import sbt._
 class MyScalaStuff(info: ProjectInfo) extends DefaultProject(info) {
 
   val essentials = "org.angelsmasterpiece" % "scala-essentials" % "1.0-SNAPSHOT"
+  val scalazCore = "com.googlecode.scalaz" %% "scalaz-core" % "5.1-SNAPSHOT"
+  
 
   val amRepo = "angelsmasterpiece repo" at "https://github.com/OlegIlyenko/angelsmasterpiece-maven-repo/raw/master"
+  val scalaToolsSnapshots = "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots/"
 
+  override def consoleInit =
+    """
+    import scalaz._
+    import Scalaz._
+    import org.am.scala.stuff._
+    import org.am.scala.essentials._
+    """
 }
---
+++ src/main/scala/Stuff.scala
@@ -0,0 +1,7 @@
+package org.am.scala
+
+package object stuff {
+    def test = "This is package object test"
+}
+
+
